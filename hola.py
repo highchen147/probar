2cd2f4772d5961adeba70a3f86e5b427942f21e9
Rodrigo Castillo <79603625+highchen147@users.noreply.github.com>
2023-04-17 08:46:23 -0600

    cambios

--- hola.py
+++ hola.py
@@ -1,2 +1,13 @@
 import pandas as pd
-print("Hola mundoi")
+import numpy as np
+
+print("Hola mundo")
+
+x = np.arange(0,100,2)
+
+# Este es un comentario de prueba
+with open("nuevo3.txt", "w") as file:
+    for i in x:
+        print(i, file=file)
+
+# Este es otro comentario de prueba 
